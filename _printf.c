f60a9ec63850ce21bcf48f1aea31671e6b38bf3d
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-03-25 22:07:54 +0000

    edge case: % followed by non c or s

--- _printf.c
+++ _printf.c
@@ -11,7 +11,7 @@
 int _printf(const char * const format, ...)
 {
 	unsigned int i = 0, j = 0;
-	int checker;
+	int checker, notfound;
 	ssize_t nob =  0;
 	va_list pars;
 	operation_t ops[] = {
@@ -29,6 +29,7 @@ int _printf(const char * const format, ...)
 		if (format[j] == '%')
 		{
 			i = 0;
+			notfound = 1;
 			while (ops[i].op)
 			{
 				if (*(ops[i].op) == format[j + 1])
@@ -38,8 +39,14 @@ int _printf(const char * const format, ...)
 						return (-1);
 					nob += checker;
 					j++;
+					notfound = 0;
 				}
 				i++;
+			}
+			if (notfound == 1)
+			{
+				nob += write(1, "%", 1);
+				j++;
 			}
 		}
 		else
