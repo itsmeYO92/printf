04be8df6d070353803d2e4433f378634e8338e47
ED DYYANY AYOUB <eddyyanyayoub@gmail.com>
2023-03-26 07:06:57 -0700

    Update printf - Fix the case of multiple %.

--- _printf.c
+++ _printf.c
@@ -1,7 +1,4 @@
 #include "main.h"
-
-
-
 /**
  * _printf - variadic function to print (simillar to printf)
  * @format: list of types
@@ -10,14 +7,12 @@
 
 int _printf(const char * const format, ...)
 {
-	int n_bytes =  0;
-	int j;
+	int n_bytes =  0, j;
 	va_list pars;
 	int (*f)(va_list);
 
 	if (!format || (format[0] == '%' && format[1] == '\0')) /* If format is NULL*/
 		return (-1);
-
 	va_start(pars, format);
 	for (j = 0; format[j] != '\0'; j++)
 	{
@@ -37,13 +32,18 @@ int _printf(const char * const format, ...)
 					j++;
 				}
 			}
+			else/* Fix the case of multiple % if more than two show up print them all*/
+			{
+				write(1, &format[j + 1], 1);
+				n_bytes++;
+				j++;
+			}
 		}
 		else
 		{
 			write(1, &format[j], 1);
 			n_bytes++;
 		}
-
 	}
 	va_end(pars);
 	return (n_bytes);
