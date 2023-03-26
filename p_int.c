8a29166c10c59a893dd273f52ab224a53b6c0bfb
ED DYYANY AYOUB <eddyyanyayoub@gmail.com>
2023-03-26 12:51:01 -0700

    Update printf - Fix p_int.

--- p_int.c
+++ p_int.c
@@ -1,40 +1,51 @@
 #include "main.h"
 
 /**
- * p_char - print a charachter
- * @args: char to print
- * Return: number of byte printed
-*/
-int print_number(int n)
+* p_int - print integers
+* @x: integer to print
+* Return: number of byte printed
+**/
+int p_int(va_list x)
 {
-	int count = 0;
-	if (n < 0)
+	int i = 1, j, divi = 1, n = va_arg(x, int), nn = 0;
+	unsigned int m = n;
+	unsigned int bck = n;
+
+	if (n >= 0 && n <= 9)
+		_putchar('0' + n);
+	else if (n < 0 && n >= -9)
 	{
-		count +=_putchar('-');
-		if (n >= -9)
-			count +=_putchar(-1 * n + '0');
-		else
-		{
-			print_number(-1 * (n / 10));
-			count +=_putchar(-1 * (n % 10) + '0');
-		}
+		n = -n;
+		i++;
+		_putchar('-');
+		_putchar('0' + n);
 	}
 	else
 	{
-		if (n  < 10)
-			count +=_putchar(n + '0');
-		else
+		if (n <= -10)
 		{
-			print_number(n / 10);
-			count +=_putchar(n % 10 + '0');
+			n = -n, nn = 1;
+			_putchar('-');
+			bck = n;
+			m = n;
+		}
+		while (m >= 10)
+		{
+			m = m / 10;
+			i++;
+		}
+		for (j = i; j > 1; j--)
+			divi = divi * 10;
+		for (j = 1; j <= i; j++)
+		{
+			m = bck / divi;
+			bck = bck - (m * divi);
+			divi = divi / 10;
+			_putchar('0' + m);
 		}
 	}
-	return (count);
-}
-
-int p_int(va_list args)
-{
-	int n = va_arg(args, int);
-	return (print_number(n));
+	if (nn == 1)
+		return (i + 1);
+	return (i);
 }
 
